8e9dc72aab6079d1b8828f4b4963c93b5613af77
souvikjs01 <souvik741156@gmail.com>
2025-09-03 18:17:05 +0530

    product model

--- model/product.go
+++ model/product.go
@@ -7,23 +7,14 @@ import (
 type Product struct {
 	ID         primitive.ObjectID `bson:"_id" json:"_id"`
 	UserID     primitive.ObjectID `bson:"user_id" json:"user_id"`
-	Title      string             `json:"title" validate:"required"`
-	Desc       string             `json:"desc" validate:"required"`
-	Img        string             `json:"img" validate:"required"`
-	Categories []string           `json:"categories" validate:"required"`
-	Size       []string           `json:"size" validate:"required"`
-	Color      []string           `json:"color" validate:"required"`
-	Price      int                `json:"price" validate:"required"`
-	InStock    bool               `json:"instock" validate:"required"`
-}
-
-type UpdateProduct struct {
-	Title   string   `json:"title"`
-	Desc    string   `json:"desc"`
-	Img     string   `json:"img"`
-	Price   int      `json:"price"`
-	Size    []string `json:"size"`
-	InStock bool     `json:"instock"`
+	Title      string             `json:"title"`
+	Desc       string             `json:"desc"`
+	Img        string             `json:"img"`
+	Categories []string           `json:"categories"`
+	Size       []string           `json:"size"`
+	Color      []string           `json:"color"`
+	Price      int                `json:"price"`
+	InStock    bool               `json:"instock"`
 }
 
 func NewProduct(title *string, description *string, image *string, categories *[]string, size *[]string, color *[]string, price *int, inStock *bool, userId *primitive.ObjectID) *Product {
