db9a911029b2c6ec22630ba3fc5dd3e88cccc451
souvikjs01 <souvik741156@gmail.com>
2025-09-04 21:53:12 +0530

    request payload

--- request/requestPayload.go
+++ request/requestPayload.go
@@ -1,5 +1,9 @@
 package request
 
+import (
+	"github.com/souvikjs01/go-ecommerce/model"
+)
+
 type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -20,4 +24,32 @@ type UpdateRequest struct {
 	FirstName    *string `json:"firstName"`
 	LastName     *string `json:"lastName"`
 	ProfileImage *string `json:"profileImage"`
+}
+
+type ProductPayload struct {
+	Title      string   `json:"title" binding:"required"`
+	Desc       string   `json:"desc" binding:"required"`
+	Img        string   `json:"img" binding:"required"`
+	Categories []string `json:"categories" binding:"required"`
+	Size       []string `json:"size" binding:"required"`
+	Color      []string `json:"color" binding:"required"`
+	Price      int      `json:"price" binding:"required"`
+	InStock    bool     `json:"instock" binding:"required"`
+}
+
+type UpdateProductPayload struct {
+	Title      *string   `json:"title"`
+	Desc       *string   `json:"desc"`
+	Img        *string   `json:"img"`
+	Categories *[]string `json:"categories"`
+	Size       *[]string `json:"size"`
+	Color      *[]string `json:"color"`
+	Price      *int      `json:"price"`
+	InStock    *bool     `json:"instock"`
+}
+
+type CreateOrderPayload struct {
+	Products []model.ProductInfo `json:"products" binding:"required"`
+	Address  string              `json:"address" binding:"required,min=4,max=20"`
+	Status   string              `json:"status" binding:"required"`
 }
